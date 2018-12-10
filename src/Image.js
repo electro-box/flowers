a1891e41806229f83b7658d78b71fd6d21528707
spaceman <wink-7@hotmail.com>
2018-12-10 23:06:33 +0700

    Added delete function

--- src/Image.js
+++ src/Image.js
@@ -3,18 +3,24 @@ import React from 'react';
 class Image extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {title: this.props.title, url: this.props.url};
+    this.onDelete = this.onDelete.bind(this);
+    this.state = {title: this.props.title, url: this.props.url, delete: false};
   }
-  
+
+  onDelete = () => {
+    this.setState({delete: true});
+  }
+
   render() {
     return (
       <div className="wrapper">
           <div className = "topbar">
               <div className="title">{this.state.title}</div>
-              <div className="delete">Delete</div>
+              <div className="delete" onClick={this.onDelete}>Delete</div>
           </div>
           <div className = "image">
               <img className = "picture" src = {this.state.url} alt = {this.state.title} />
+              {this.state.delete && <div className="deleted">Deleted</div>}
           </div>
       </div>
     );
